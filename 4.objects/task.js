350263470f3855239e4cd48b68196bb6d775bda4
Nikita Sokolov <nikita436@gmail.com>
2024-12-19 22:43:52 +0700

    Четвертое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,49 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty("marks")) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  let avrge = this.marks.reduce((sum, value, index, marks) => {
+    sum += value;
+        if (index === marks.length - 1) {
+      return sum / marks.length;
+    }
+
+    return sum;
+  },0);
+  return avrge;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
+
+new Student("Василиса", "женский", 19);
+new Student("София", "женский", 21);
+new Student("Артём", "мужской", 25);
+
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
